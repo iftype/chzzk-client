a941a07ab154040aadbb164335a222f14ab40fb9
iftype <iftype@naver.com>
2025-11-23 00:43:39 +0900

    feat(API): 서버의 API와 연결

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,52 @@
-const apiUrl = "http://localhost:4000/test";
+const BASE_URL = "http://146.56.118.89";
 
-export async function fetchStreamerData() {
+export async function fetchStreamer() {
   try {
-    const response = await fetch(apiUrl);
+    console.log("API호출");
+
+    const url = `${BASE_URL}/channel`;
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const content = await response.json();
-    console.log("API 호출 성공:", content);
-    return content;
+    const { data } = content;
+    return data;
+  } catch (error) {
+    console.error("API 호출 중 오류 발생:", error);
+    return null;
+  }
+}
+
+export async function fetchStreamerData(channelId) {
+  try {
+    console.log("API호출");
+    const url = `${BASE_URL}/channel/${channelId}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const content = await response.json();
+    const { data } = content;
+    return data;
+  } catch (error) {
+    console.error("API 호출 중 오류 발생:", error);
+    return null;
+  }
+}
+
+export async function fetchStreamerLogData(channelId, date) {
+  try {
+    console.log("API호출");
+    const url = date ? `${BASE_URL}/log/${channelId}?date=${date}` : `${BASE_URL}/log/${channelId}`;
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const content = await response.json();
+    const { data } = content;
+    return data;
   } catch (error) {
     console.error("API 호출 중 오류 발생:", error);
     return null;
