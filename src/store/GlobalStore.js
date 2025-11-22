ca16531c18f5d1ea1d2b35043168888bdfc95b33
iftype <iftype@naver.com>
2025-11-23 00:44:29 +0900

    feat(GlobalStore): 전역 저장소 구현

---
+++ src/store/GlobalStore.js
@@ -0,0 +1,37 @@
+import { fetchStreamer } from "../api/api.js";
+
+class GlobalStore {
+  constructor() {
+    this.state = {
+      channelList: new Map(),
+      channelListLoaded: false,
+    };
+  }
+
+  async loadChannelList() {
+    if (this.state.channelListLoaded) return this.state.channelList;
+
+    const channelList = await fetchStreamer();
+
+    const map = new Map();
+    channelList.forEach((channel) => {
+      map.set(channel.channelId, channel);
+    });
+    console.log(channelList);
+
+    this.state.channelList = map;
+    this.state.channelListLoaded = true;
+
+    return map;
+  }
+
+  getChannelList() {
+    return this.state.channelList;
+  }
+
+  getChannel(key) {
+    return this.state.channelList.get(key) || null;
+  }
+}
+
+export default new GlobalStore();
