41f94ffd2353bcac76203ecf8cdba766e46f4af1
iftype <iftype@naver.com>
2025-11-23 10:55:17 +0900

    feat(LiveLogContainer): VOD 확인 span 추가

--- src/components/LiveLogContainer.js
+++ src/components/LiveLogContainer.js
@@ -8,15 +8,18 @@ export default class LiveLogContainer extends Component {
       logTime: formatDateYM(this.props.liveLog[0].logTime) || null,
       openDate: this.props.liveLog[0].openDate || null,
       closeDate: formatDateYM(this.props.liveLog[0].closeDate) || null,
+      videoNo: this.props.liveLog[0].video.videoNo || null,
     };
   }
 
   template() {
-    const { logTime, closeDate } = this.state;
+    const { logTime, closeDate, videoNo } = this.state;
+    console.log(videoNo);
     return `
     <div class="live-log-card-date">
       ${this.checkToday() ? "TODAY" : logTime}
       ${!closeDate ? "<span class='live-text'>LIVE</span>" : ""}
+      ${videoNo ? "<span class='live-text-video'>VOD</span>" : ""}
     </div>
     <div id="live-log-card"></div>
     `;
